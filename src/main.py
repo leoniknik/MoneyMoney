f2b60e0d6fa1280af27ce606731025058332c71b
Lavifae <9751@mail.ru>
2016-10-31 15:05:09 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,36 @@
+from mmhandler import MmHandler
+#from sql import SQL
+import config
+import telebot
 """
 TODO:
-Класс 1
-1) Добавить постоянный источник  дохода(дата,сумма,название)
-2) Добавить доход(сумма, название)
-3) Добавить категорию(название)
-4) Вывести категории()
-5) Вывести источники дохода()
-6) Добавить расход(категория, сумма)
-7) Получить отчет за год/неделю/месяц(категория или пусто)
-8) Показать сегодняшние операции
-9) Получить отчет за период(дата,дата,категория или пусто)
-
-OPTIONALLY  Оповещения (Ты забыл обо мне?) 
-
-10) Разобрать выражение на естественном языке
-
-Класс 2 
-БД - processing
-
+Оповещения (Ты забыл обо мне?)
+Разобрать выражение на естественном языке
 11) start
 12) help
 13) main
 14) формирование ответного сообщения
-
+прикрутить передачу trace ошибок из SQL
 """
+
+bot = telebot.TeleBot(config.token)
+handler = MmHandler(0) # по умолчанию user_id = 0
+# прикрутить вебхуки
+# прикрутить разбор сообщения на естественном языке
+
+# для тестирования
+@bot.message_handler(commands=['start'])
+def start(message):
+    MmHandler.user_id = message.chat.id
+    if (MmHandler.start):
+        bot.send_message(message.chat.id, 'Hello. Let\'s start from help command')
+    else:
+        bot.send_message(message.chat.id, 'Undefined error')
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    pass
+
+# бесконечная петля опроса
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
