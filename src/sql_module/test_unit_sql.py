24586ccbdfd1b13b9669c5a0d15f4527d81af5d7
leoniknik <your_email@whatever.com>
2016-11-01 18:23:56 +0300

    18:23

--- src/sql_module/test_unit_sql.py
+++ src/sql_module/test_unit_sql.py
@@ -1,30 +1,18 @@
+from sql_module.sql import SQL
 import unittest
 
 
-class SQL_Test(unittest.TestCase):
+class SQLTest(unittest.TestCase):
     def setUp(self):
         self.sql = SQL()
 
-    def test_add(self):
-        self.alchemy.add(Item("C"))
-        self.alchemy.add(Item("F"))
-        self.assertEqual(len(self.alchemy.items), 2)
+    def test_add_user(self):
+        user_id = 1
+        self.sql.execute_query("START TRANSACTION;")
+        self.sql.add_user(user_id)
+        self.assertEqual(self.sql.execute_query("SELECT id FROM user WHERE id={}".format(user_id))[0][0], 1)
+        self.sql.execute_query("ROLLBACK;")
+        self.sql.close()
 
-    def test_remove(self):
-        item = Item("C")
-        self.alchemy.add(item)
-        self.assertEqual(len(self.alchemy.items), 1)
-        self.alchemy.remove(item)
-        self.assertEqual(len(self.alchemy.items), 0)
-
-    def test_boom(self):
-        item1 = Item("Na", reacts_with=["H2O"])
-        item2 = Item("H2O", reacts_with=["Na"])
-        self.alchemy.add(item1)
-        self.assertRaises(BoomException, self.alchemy.add, item2)
-        self.assertEqual(len(self.alchemy.items), 0)
-
-
-# Обычно в реальных проектах использует механизм автоматического нахождения тестов (discover).
-suite = unittest.defaultTestLoader.loadTestsFromTestCase(AlchemyTest)
+suite = unittest.defaultTestLoader.loadTestsFromTestCase(SQLTest)
 unittest.TextTestRunner().run(suite)
