621d7cc55f0097e5f838424a5ce27a9be4c3b1f3
Lavifae <9751@mail.ru>
2016-12-19 17:27:48 +0300

    add exception and rewrite report function

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot(config.token)
 handler = MmHandler(0)  # по умолчанию user_id = 0
 help_message = "добавить доход:\n+{сумма} {категория} {комментарий}\nдобавить расход:\n-{сумма} {категория} {комментарий}\nпоказать категории:\nпокажи категории {расходов} {доходов}\nдобавить категорию:\nдобавь категорию {название}\nудалить категорию:\nудали категорию {название}\nпосмотреть отчет за месяц:\nотчет за {месяц}\nпосмотреть отчет за определенный период:\nотчет с {начальная дата} по {конечная дата}\nформат даты: xx-xx-xxxx"
 report_periods = {'день', 'неделю', 'месяц', 'год'}
+category_mods = {'расходов', 'доходов'}
+format_error = Exception('Неправильный формат команды!')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -28,6 +30,8 @@ def parse(message):
     try:
         str_array = message.text.lower().split()
         length = len(str_array)
+        
+        # if empty line
         if length == 0:
             bot.send_message(message.chat.id, 'Забыл список команд? Держи:')
             bot.send_message(message.chat.id, help_message)
@@ -38,6 +42,8 @@ def parse(message):
                 if re.match('[+]\d+', str_array[0]) or re.match('[-]\d+', str_array[0]):
                     handler_message = handler.add_operation(int(str_array[0]))
                     bot.send_message(message.chat.id, handler_message)
+                else:
+                    raise format_error
                 
             elif length >= 2 and re.match('[а-яa-zА-ЯA-Z]+', str_array[1]):
                 if re.match('[+]\d+', str_array[0]) or re.match('[-]\d+', str_array[0]):
@@ -47,28 +53,47 @@ def parse(message):
                     else:
                         handler_message = handler.add_operation(int(str_array[0]), str_array[1])
                     bot.send_message(message.chat.id, handler_message)
+                else:
+                    raise format_error
+                       
+        elif length >= 2 and str_array[0] == 'покажи' and str_array[1] == 'категории':
+            if length == 3:
+                if str_array[2] in category_mods:
+                    handler_message = handler.show_categories(str_array[2])
+                else:
+                    raise Exception('Неправильный формат команды!')
+            else:
+                handler_message = handler.show_categories()
+                bot.send_message(message.chat.id, handler_message)
+            
+        elif length == 3 and and str_array[1] == 'категорию' and re.match('[а-яa-zA-ZА-Я]+', str_array[2]):
+            if str_array[0] == 'удали':
+                handler_message = handler.del_category(str_array[2])
+                bot.send_message(message.chat.id, handler_message)
                 
-        elif str_array[0] == 'покажи' and str_array[1] == 'категории':
-            handler_message = handler.show_categories()
-            bot.send_message(message.chat.id, handler_message)
+            elif str_array[0] == 'добавь':
+                handler_message = handler.add_category(str_array[2])
+                bot.send_message(message.chat.id, handler_message)
+            else:
+                raise format_error
             
-        elif len(str_array) == 3 and str_array[0] == 'добавь' and str_array[1] == 'категорию': #and re.match('[а-яa-z]+', str[2]):
-            handler_message = handler.add_category(str_array[2])
-            bot.send_message(message.chat.id, handler_message)
-            
-        elif str_array[0] == 'удали' and str_array[1] == 'категорию': #and re.match('[а-яa-z]+', str[2]):
-            handler_message = handler.del_category(str_array[2])
-            bot.send_message(message.chat.id, handler_message)
-            
-        elif len(str_array) >= 2 and str_array[0] == 'отчет':
+        elif length >= 3 and str_array[0] == 'отчет' and :
             if str_array[1] == 'за' and (str_array[2] in report_periods):
                 handler_message = handler.view_report(str_array[2])
                 bot.send_message(message.chat.id, handler_message)
-                # отчет за период
-            elif str_array[1] == 'с' and re.match('\d{1,2}-\d{1,2}-\d{4}', str_array[2]) and str_array[3] == 'по' and \
-                    re.match('\d{1,2}-\d{1,2}-\d{4}', str_array[4]):
-                handler_message = handler.view_custom_report(str_array[2], str_array[4])
+                
+            elif str_array[1] == 'с' and re.match('\d{1,2}-\d{1,2}-\d{4}', str_array[2]):
+                date_from_split_reverse = str_array[2].split('-')[::-1]
+                date_from = '-'.join(date_from_split_reverse)
+                if length >= 4 and str_array[3] == 'по' and re.match('\d{1,2}-\d{1,2}-\d{4}', str_array[4]):
+                    date_to_split_reverse = str_array[4].split('-')[::-1]
+                    date_to = '-'.join(date_to_split_reverse)
+                    handler_message = handler.view_custom_report(date_from, date_to)
+                else:
+                    handler_message = handler.view_custom_report(date_from)
                 bot.send_message(message.chat.id, handler_message)
+            else:
+                raise format_error
 
         else:
             bot.send_message(message.chat.id, 'Не знаю такой команды! Список моих команд:')
